7defca6139c116fd61a663df627ca0290cbbbacd
mayankjai528-ui <mayankjai528@gmail.com>
2025-09-21 22:55:56 +0530

    task1.py

--- task1.py
+++ task1.py
@@ -1,6 +1,5 @@
-a=float(input("Enter the first number:"))
-b=float(input("Enter the second number:"))
-print("Addition:",a+b)
-print("Subtraction:",a-b)
-print("Multiplication:",a*b)
-print("Division:",a/b)
+a=int(input("Enter a number:"))
+if a%2!=0:
+    print(f'{a} is an odd number.')
+else:
+    print(f'{a} is an even number.')
